d46104e754ffe56d68fc75baa3ec672a17f80d38
Graeme Parkes <graeme.parkes3@gmail.com>
2026-07-26 20:18:18 -0700

    Remove staging migration bridge

--- server.js
+++ server.js
@@ -2,26 +2,8 @@ const {
   reportTargetStartupFailure,
   startTargetProcess,
 } = require("./src/bootstrap/startTargetProcess");
-const {
-  runStagingSchemaMigrationBridge,
-} = require("./src/bootstrap/runStagingSchemaMigrationBridge");
 
-async function main() {
-  const migration = await runStagingSchemaMigrationBridge();
-  if (migration.ran) {
-    process.stdout.write(
-      `${JSON.stringify({
-        event: "staging_schema_migration.completed",
-        backupId: migration.backupId,
-        manifestChecksum: migration.manifestChecksum,
-        schemaVersion: migration.schemaVersion,
-      })}\n`
-    );
-  }
-  return startTargetProcess();
-}
-
-main().catch((error) => {
+startTargetProcess().catch((error) => {
   reportTargetStartupFailure(error);
   process.exitCode = 1;
 });
